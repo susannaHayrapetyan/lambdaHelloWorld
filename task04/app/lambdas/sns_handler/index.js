6e3af836b2636b210b4ffe141dcf5338e730f229
Susanna Hayrapetyan <susannahayrapetyan@Susannas-MacBook-Pro.local>
2025-01-08 14:48:02 +0400

    log sns message

--- task04/app/lambdas/sns_handler/index.js
+++ task04/app/lambdas/sns_handler/index.js
@@ -1,5 +1,10 @@
 exports.handler = async (event) => {
-    console.log('SNS_EVENT', event);
+    console.log('SNS_EVENT', JSON.stringify(event));
+    
+    event?.Records?.forEach(record => {
+        console.log('SNS_EVENT_record', record?.Sns?.[0]);
+    });
+
     // TODO implement
     const response = {
         statusCode: 200,
